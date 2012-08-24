7c222d72c0a65d1e63119ecf20fb36a3d4415a24
Marcel Eichner <love@ephigenia.de>
2012-08-24 19:37:26 +0200

    [fixed #38] by updating flattr embed code to html5 button and also including flattr js via app.js

--- public/wp-content/themes/bg/elements/teaser/hund.php
+++ public/wp-content/themes/bg/elements/teaser/hund.php
@@ -1,10 +1,15 @@
 <li class="teaser" style="font-weight:bold; font-size: 1.2em; line-height: 1.2em;">
-	<img title="Spenden" src="http://berlinergazette.de/wp-content/uploads/bg_dog_185px_white_transparent.png" alt="Spenden" /><br>
-	Hallo Leser, würdest du uns dein Geld geben? Warum? Lies <a href="http://berlinergazette.de/aufruf-spenden/">hier</a>.</a><br>
-	Ansonsten: Ein Klick auf den grünen Knopf unten genügt! Wie das geht, steht <a href="http://berlinergazette.de/was-ist-flattr/">hier</a>.
-	<div style="clear: both; display: table; margin: 0.75em 0em 0em 0em;">
-		<a class="FlattrButton" style="display:none;" rev="flattr;button:compact;" href="https://flattr.com/profile/BerlinerGazette"></a>
-		<noscript><a href="http://flattr.com/thing/336467/BerlinerGazette-on-Flattr" target="_blank">
-		<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>
-	</div>
+	<img title="Spenden" src="http://berlinergazette.de/wp-content/uploads/bg_dog_185px_white_transparent.png" alt="Die Berliner Gazette durch Spenden unterstützen" />
+	<p>
+		Hallo Leser, würdest du uns dein Geld geben? Warum? Lies <a href="http://berlinergazette.de/aufruf-spenden/">hier</a>.</a><br>
+		Ansonsten: Ein Klick auf den grünen Knopf unten genügt! Wie das geht, steht <a href="http://berlinergazette.de/was-ist-flattr/">hier</a>.
+	</p>
+	<a class="FlattrButton" style="display:none;"
+			title="<?= bloginfo('name') ?>"
+			data-flattr-uid="BerlinerGazette"
+			data-flattr-category="text"
+			href="https://flattr.com/profile/BerlinerGazette">
+	</a>
+	<noscript><a href="http://flattr.com/thing/336467/BerlinerGazette-on-Flattr" target="_blank">
+	<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>
 </li>
--- public/wp-content/themes/bg/js/source/app.js
+++ public/wp-content/themes/bg/js/source/app.js
@@ -58,14 +58,24 @@ $(document).ready(function() {
 		return true;
 	});
 
+	// Flattr Button integration
+	var s = document.createElement('script');
+	var t = document.getElementsByTagName('script')[0];
+	s.type = 'text/javascript';
+	s.async = true;
+	s.src = '//api.flattr.com/js/0.6/load.js?'+
+			'mode=auto&uid=BerlinerGazette&language=de_DE&category=text';
+	t.parentNode.insertBefore(s, t);
+
 });
+
 
 // google analytics
 var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-20944588-1']);
   _gaq.push(['_trackPageview']);
   (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
