5e5989e4263c23d7511f642203278e9c8b15b487
Marcel Eichner <love@ephigenia.de>
2012-09-14 12:06:33 +0200

    Added english for search form when in forum

--- public/wp-content/themes/bg/elements/searchform.php
+++ public/wp-content/themes/bg/elements/searchform.php
@@ -1,10 +1,23 @@
+<?php
+// Forum 
+if (function_exists('is_bbpress') && is_bbpress()) {
+	$postTypes = array(
+		'topic',
+		'reply'
+	);
+	$searchQueryPlaceholder = 'Enter keyword';
+	$searchSubmitLabel = 'Search';
+} else {
+	$searchQueryPlaceholder = 'Suchbegriff eingeben';
+	$searchSubmitLabel = 'Finden';
+}
+?>
 <form method="get" id="searchForm" action="<?php bloginfo('home'); ?>/">
 	<fieldset>
-		<?php if (function_exists('is_bbpress') && is_bbpress()) { ?>
-		<input type="hidden" name="post_type[]" value="topic" />
-		<input type="hidden" name="post_type[]" value="reply" />
+		<?php if (!empty($postTypes)) foreach($postTypes as $postType) { ?>
+		<input type="hidden" name="post_type[]" value="<?= $postType ?>" />
 		<?php } ?>
-		<input type="text" class="search_input input" value="Suchbegriff eingeben" name="s" size="21" />
-		<input type="submit" class="search_submit submit" id="searchsubmit" value="Finden" />
+		<input type="text" class="search_input input" value="<?= htmlentities($searchQueryPlaceholder) ?>" name="s" size="21" />
+		<input type="submit" class="search_submit submit" id="searchsubmit" value="<?= htmlentities($searchSubmitLabel) ?>" />
 	</fieldset>
 </form>
