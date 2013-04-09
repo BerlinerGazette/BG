fa4ba416c08270d66a4869ab29ca6a32666107a8
Marcel Eichner <love@ephigenia.de>
2013-04-09 11:23:42 +0200

    fix bug where bbpress 2.2.1 was not showing the content

--- public/wp-content/themes/bg/bbpress.php
+++ public/wp-content/themes/bg/bbpress.php
@@ -2,7 +2,7 @@
 <?php require TEMPLATEPATH.'/sidebar.php'; ?>
 <div id="center" class="bbpress" role="main">
 	<?php do_action( 'bbp_template_notices' ); ?>
-	<?php the_content(); ?>
+	<?php the_post(); the_content(); ?>
 </div>
 <?php require TEMPLATEPATH.'/sidebar2.php'; ?>
 <?php get_footer(); ?>
